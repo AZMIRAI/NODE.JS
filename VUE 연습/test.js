32e358412b3dd1433b014738d9dbc9a36d6a6331
KRHDY <1901322@g.yju.ac.kr>
2021-07-20 14:17:32 +0900

    node.js

--- VUE 연습/test.js
+++ VUE 연습/test.js
@@ -1,6 +1,14 @@
 const http = require('http')
 
+// http라는 함수를 받아옴
+
 const server = http.createServer	((req, res)=>{res.end('hello')
 })
 
+// => 람다식 
+// res.end('??')
+// createServer 라는 객체
+
+
 server.listen(3000)
+// 마지막으로 3000번
